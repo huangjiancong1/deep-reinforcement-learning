c94860be7be847264fa45db0f0181ba11bc93a92
Jim Huang <374729746@qq.com>
2019-01-15 11:38:57 +0800

    Update agent.py

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,17 +1,27 @@
 import numpy as np
 from collections import defaultdict
+import random
+import pdb
+
 
 class Agent:
 
-    def __init__(self, nA=6):
+    def __init__(self, nA=6, alpha=0.01, gamma=0.99, eps=0.8):
         """ Initialize agent.
 
         Params
         ======
         - nA: number of actions available to the agent
         """
+        ##self.nA = nA
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.alpha = alpha
+        self.gamma = gamma
+        self.eps = eps 
+       
+        
+        
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,8 +34,19 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
-
+        ####################
+        ## epsilon_greedy ##
+        #################### 
+        if random.random() > self.eps: # select greedy action with probability epsilon
+            return np.argmax(self.Q[state])
+        else:                     # otherwise, select an action randomly
+            return np.random.choice(self.nA)
+        from monitor import i_episode
+        pdb.set_trace()
+        self.eps = eps / i_episode 
+        
+        
+        
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
 
@@ -37,4 +58,21 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        
+        ##################
+        # Expected Sarsa #
+        ##################
+#         # use epsilon-greedy to select the next action from next state
+#         next_action = select_action(next_state)  
+        
+        current = self.Q[state][action]  # estimate in Q-table (for current state, action pair)
+        
+#         # get value of state, action pair at next time step
+#         Qsa_next = np.max(self.Q[next_state])if next_state is not None else 0    
+        
+        policy_s = np.ones(self.nA) * self.eps / self.nA  # current policy (for next state S')
+        policy_s[np.argmax(self.Q[next_state])] = 1 - self.eps + (self.eps / self.nA) # greedy action
+        Qsa_next = np.dot(self.Q[next_state], policy_s)         # get value of state at next time step
+        
+        target = reward + (self.gamma * Qsa_next)               # construct TD target
+        self.Q[state][action] += current + (self.alpha * (target - current))# get update value
